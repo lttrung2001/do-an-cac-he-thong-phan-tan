d6b74c23c2b1e6a95c17bf680d69a195d0017c22
Le Thanh Trung <lt.trung2001@gmail.com>
2022-12-12 14:56:39 +0700

    add giao diện + nhắn nhóm

--- WebApplication1/Hubs/ChatHub.cs
+++ WebApplication1/Hubs/ChatHub.cs
@@ -3,10 +3,41 @@ namespace WebApplication1.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task<Task> OnConnected()
+        public async Task Join(string username)
         {
-            await Clients.All.SendAsync("NewConnection", Context.ConnectionId);
-            return base.OnConnectedAsync();
+            string id = Context.ConnectionId;
+            await Clients.Client(id).SendAsync("GetOnlines", User.Users);
+            await Clients.AllExcept(id).SendAsync("Join", id, username);
+            User.Users.Add(new User { Id = id, Name = username });
+        }
+
+        public async Task Leave(string username)
+        {
+            string id = Context.ConnectionId;
+            foreach (var user in User.Users)
+            {
+                if (user.Id == id)
+                {
+                    User.Users.Remove(user);
+                    break;
+                }
+            }
+            await Clients.AllExcept(id).SendAsync("Leave", Context.ConnectionId, username);
+        }
+
+        public async Task<Task> OnDisconnected()
+        {
+            string id = Context.ConnectionId;
+            foreach (var user in User.Users)
+            {
+                if (user.Id == id)
+                {
+                    User.Users.Remove(user);
+                    break;
+                }
+            }
+            await Clients.AllExcept(id).SendAsync("Leave", Context.ConnectionId, "");
+            return base.OnDisconnectedAsync(new Exception());
         }
 
         public async Task SendMessage(string user, string message)
@@ -14,9 +45,23 @@ namespace WebApplication1.Hubs
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
 
-        public async Task SendPrivateMessage(string user, string message, string receiver)
+        public async Task SendPrivateMessage(string user, string message, string receiverUser)
         {
-            await Clients.Client(receiver).SendAsync("PrivateMessage", user, message);
+            foreach (var u in User.Users)
+            {
+                if (u.Name.ToLower() == receiverUser.ToLower())
+                {
+                    await Clients.Client(u.Id).SendAsync("PrivateMessage", user, message);
+                    break;
+                }
+            }
         }
+    }
+    class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public static List<User> Users = new List<User>();
     }
 }
--- WebApplication1/wwwroot/js/chat.js
+++ WebApplication1/wwwroot/js/chat.js
@@ -1,39 +1,105 @@
 ﻿"use strict";
 
-var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();0
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+const toNodes = html =>
+    new DOMParser().parseFromString(html, 'text/html').body.childNodes[0]
+
+var userElement = (id, name) => {
+    return `<li id="${id}" class="clearfix">
+                            <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="avatar">
+                            <div class="about">
+                                <div class="name">${name}</div>
+                                <div class="status"> <i class="fa fa-circle online"></i> online </div>
+                            </div>
+                        </li>`
+}
+
+var myMessage = (message) => {
+    return `<li class="clearfix">
+                                <div class="message other-message float-right"> ${message} </div>
+                            </li>`
+}
+
+var otherMessage = (user, message) => {
+    return `<li class="clearfix">
+                                <div class="message-data">
+                                    <span class="message-data-time">${user}</span>
+                                </div>
+                                <div class="message my-message">${message}</div>
+                            </li>`
+}
 
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
+document.getElementById("btnLeave").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
     var li = document.createElement("li");
     document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
-    // should be aware of possible script injection concerns.
     li.textContent = `${user} says ${message}`;
+    var myUser = document.getElementById("userInput").value;
+    var ul = document.getElementById('chat-history-container');
+    if (user === myUser) {
+        ul.append(toNodes(myMessage(message)));
+    } else {
+        ul.append(toNodes(otherMessage(user, message)));
+    }
 });
 
-connection.on("NewConnection", function (id) {
-    alert(`${id} has joined.`);
+document.getElementById("btnJoin").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    if (user != null && user != '') {
+        connection.start().then(function () {
+        connection.invoke("Join", user);
+            connection.on('GetOnlines', (args) => {
+                console.log(args);
+            var userList = document.getElementById('user-list');
+            for (var i = 0; i < args.length; i++) {
+                userList.append(toNodes(userElement(args[i].id, args[i].name)));
+            }
+        });
+        }).catch(function (err) {
+            return console.error(err.toString());
+        });
+        document.getElementById("sendButton").disabled = false;
+        document.getElementById("btnLeave").disabled = false;
+        document.getElementById("userInput").disabled = true;
+        document.getElementById("btnJoin").disabled = true;
+    }
+    event.preventDefault();
+});
+
+document.getElementById("btnLeave").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    connection.invoke("Leave", user).then(function () {
+        connection.stop();
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+    document.getElementById("sendButton").disabled = true;
+    document.getElementById("btnLeave").disabled = true;
+    document.getElementById("userInput").disabled = false;
+    document.getElementById("btnJoin").disabled = false;
+    event.preventDefault();
+});
+
+connection.on('Join', function (id, user) {
+    document.getElementById('user-list').append(toNodes(userElement(id, user)));
+})
+
+connection.on('Leave', function (id, user) {
+    document.getElementById(id).remove();
 })
 
 connection.on("PrivateMessage", function (user, message) {
     alert(`${user} says ${message}.`);
 })
-    
-connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
-    connection.invoke("OnConnected").catch(function (err) {
-        return console.error(err.toString());
-    })
-}).catch(function (err) {
-    return console.error(err.toString());
-});
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
+    document.getElementById("messageInput").value = "";
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
